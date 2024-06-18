2a0a696850b68b78a72b54c3d83e3846fd86e7fd
Nishu <nishanthravichandran12@gmail.com>
2024-06-18 10:08:05 +0530

    Change font

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -6,12 +6,19 @@ function Hero() {
   return (
     <div className='hero'>
       <div className='hero-image'>
-        <p className='header-slogan'>
+        <h1 className='dot-hawg-header'>
+        <span style={{"color" : "#cc4c4b", "font-family":"Chewy"}}> DOT </span> <span style={{"color" : "black", "font-family":"Chewy"}}> HAWG </span>
+
+        </h1>
+        <h2 className='header-slogan'>
           BUNS STRAIGHT <br /> FROM HEAVEN
-        </p>
+        </h2>
+        <div className='home-order'>
         <p className='order-from'> Order Now On </p>
-        <button className='swiggy-button home-order-button'> <img src='https://subway.in/static/media/swiggy.720f8f764788f0988289.webp' className='swiggy-button-image home-order-button-image' alt="Swiggy"></img> </button>
-        <button className='zomato-button home-order-button'> <img src="https://subway.in/static/media/zomato.4e0f3920926bf9e420d5.webp" className='zomato-button-image home-order-button-image' alt='Zomato'></img> </button>
+        <button className='swiggy-button home-order-button'>  </button>
+        <button className='zomato-button home-order-button'> </button>
+        </div >
+
       </div>
       <div className='hot-dog-video'>
         <div className='hot-dog-video-border'><iframe width="420" height="315"
