506607959430ac4aa610d75b1d1b7e98f27776af
Nishu <nishanthravichandran12@gmail.com>
2024-06-18 21:13:49 +0530

    Responsive menu

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MenuItem.css';
 
 const MenuItem = ({ name, imageUrl }) => {
   console.log(imageUrl)
@@ -7,16 +8,17 @@ const MenuItem = ({ name, imageUrl }) => {
       <img
         src={imageUrl}
         alt={name}
-        style={{
-          borderRadius: '20px',
-          width: '20vw', 
-          height:'20vw',
-          objectFit: 'cover',
-          // Ensures the image takes full width of its container
-          display: 'block', // Ensures the image behaves as a block element
-        }}
+        // style={{
+        //   borderRadius: '20px',
+        //   width: '40vw', 
+        //   height:'40vw',
+        //   objectFit: 'cover',
+        //   // Ensures the image takes full width of its container
+        //   display: 'block',
+        //    // Ensures the image behaves as a block element
+        // }}
       />
-      <p style={{fontSize:"2vw", marginTop: '10px',color:"white" }}>{name}</p>
+      <p>{name}</p>
     </div>
   );
 };
